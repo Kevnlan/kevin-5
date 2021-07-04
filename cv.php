c097b895e2c32b8c4a93c34aeaf7690cd03ef621
Kevin Langat <kevnlan11@gmail.com>
2021-07-04 18:50:48 +0300

    layout change again

---
+++ cv.php
@@ -0,0 +1,193 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon/favicon-16x16.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Curriculum-Vitae</title>
+</head>
+<body>
+    <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#"><img src="img/logo.jpg"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                        <a class="nav-link" href="index.php">HOME</a>
+                    </li>						
+                    <li class="nav-item">
+                        <a class="nav-link" href="cv.php">CV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#contacts">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+    <section class="introduction">
+        <div class="first container p-3 my-3  ">
+            <div class="row">
+                <div class="col-sm-4">
+                    <p><h5>Photo Maybe</h5></p>
+                    <img src="img/copy.jpg" style="width:100%; height:270px;" alt="Avatar">
+                </div>
+                <div class="col-sm-4">
+                    <p><h5>Social Media maybe</h5></p>
+
+                    <div class="socials container">
+                        <a class="links" href="https://github.com/Kevnlan" target="_blank"><i class="fab fa-github-square fa-7x"></i></a>
+                        <a class="links" href="https://www.linkedin.com/in/kevin-langat-43009191/" target="_blank"><i class="fab fa-linkedin fa-7x"></i></a>
+                        <a class="links" href="" target="_blank"><i class="fab fa-twitter-square fa-7x"></i></a>
+                    </div>
+                    
+                </div>
+                <div class="col-sm-4">
+                    <p><h5>carousel links maybe</h5></p>
+
+                    <p><i class="fa fa-briefcase fa-fw margin-right large text-black"></i>Software Developer                        </p>
+                        <p><i class="fa fa-home fa-fw margin-right large text-black"></i>Nairobi, Kenya</p>
+                        <p><i class="fa fa-envelope fa-fw margin-right large text-black"></i>langatkevin123@gmail.com                        </p>
+                        <p><i class="fa fa-phone fa-fw margin-right large text-black"></i>254773546601</p>
+                </div>
+            </div>
+            <div class="headline">
+                <p>I am developer with 2 years experience mobile development and 1 year experience in web development. I am highly motivated, dynamic and resourceful.</p>
+            </div>
+        </div>
+    </section>
+    <section class="second ">
+        <div class="container-fluid p-3 my-3  ">
+            <div class="row">
+                <div class="know col-sm-6">
+                    
+                   
+                    <div class="content">
+                    <div class="skill">
+                            <p><h3>Skills</h3></p>
+                            <div class="content">
+                                <div class="combined">
+                                    <p>Mobile Development</p>
+                                    <div class="container">
+                                        <div class="skills html">90%</div>
+                                    </div>
+                                    <p>Web Development</p>
+                                    <div class="container">
+                                        <div class="skills css">90%</div>
+                                    </div>
+                                    
+                                    <p>UI/UX</p>
+                                    <div class="container">
+                                        <div class="skills js">90%</div>
+                                    </div>
+                                    
+                                    <p>DevOps</p>
+                                    <div class="container">
+                                        <div class="skills php">90%</div>
+                                    </div>
+                                    
+                                    <p>CCNA</p>
+                                    <div class="container">
+                                        <div class="skills php">90%</div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="education">
+                            <p><h3>Education</h3></p>
+                            <div class="schools">
+                                Maseno University 2016 - 2020 <br>
+                                <ul>
+                                    <li>BSc Computer Science</li>
+                                    <li>2020 (Jan – April) CCNA 4</li>
+                                    <li>2019 (Sept-Dec) CCNA 3</li>
+                                    <li>2019 (Jan – April) CCNA 2</li>
+                                    <li>2018 (Sept-Dec) CCNA 1</li>
+                                </ul>
+                                Online <br>
+                                <ul>
+                                    <li>2021 (May – present) AWS Certified Developer Associate</li>
+                                    <li>2018 (Feb – April) Andela Learning Community 2.0 Android Development</li>
+                                    <li></li>
+                                </ul>
+                                Kericho High School 2008-2011 <br>
+                                <ul>
+                                    <li>K.C.S.E</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="title">
+                        <p><h3>Experience</h3></p>
+                    </div>
+                    <div class="content">
+                        <div class="company">
+                            Software Developer Intern<br>
+                            Sinsolut Technologies<br>
+                            Jan 2021 – April 2021<br>
+                            <ul>
+                                <li>Designing and developing mobile applications using flutter</li>
+                                <li>Integration of mobile applications with systems</li>
+                                <li>Web development using Laravel framework</li>
+                                <li>Building REST API end points using Laravel</li>
+                                <li>Problem solving</li>
+                            </ul>
+                        </div>
+                        <div class="company">
+                            ICT Intern<br>
+                            Kisumu Law Courts<br>
+                            May 2019 – July 2019<br>
+                            <ul>
+                                <li>Distributing and installing software and hardware infrastructure</li>
+                                <li>Network and IT support and troubleshooting errors</li>
+                                <li>Data entry and Capture</li>
+                                <li>Assisting in ICT helpdesk</li>
+                                <li>Routine maintenance of software and hardware equipment</li>
+                                <li>Training of users to be familiar with IT hardware and software</li>
+                                <li>Documentation and filing of ICT related activities</li>
+                            </ul>
+                        </div>
+                        <div class="company">
+                            ICT Intern<br>
+                            Geothermal Development Company<br>
+                            FEBRUARY 2016 – APRIL 2016<br>
+                            <ul>
+                                <li>IP telephony support</li>
+                                <li>Installation and deployment of new software and hardware infrastructure</li>
+                                <li>Provided technical support to users by diagnosing and correcting problems</li>
+                                <li>Conducted electrical safety checks on computer equipment</li>
+                                <li>Routine maintenance of software and hardware equipment</li>
+                                <li>Network deployment, monitoring and support</li>
+                                <li>Documentation and filing of ICT related activities</li>
+                                <li>Selection and disposal of redundant ICT equipment</li>
+                            </ul>
+                        </div>
+                        <div class="company">
+                            Sales Assistant<br>
+                                Ches Dairies<br>
+                                August 2012 – December 2012<br>
+                            <ul>
+                                <li>Packaging of dairy products</li>
+                                <li>Sales</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
